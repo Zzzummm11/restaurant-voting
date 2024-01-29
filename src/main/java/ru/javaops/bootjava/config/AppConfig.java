4e9b098c27e0c5322b2007290346610d838a2c59
Konstantin Mogilev <zzzummm272@gmail.com>
2024-01-29 23:49:25 +0400

    upgrade refactoring

--- src/main/java/ru/javaops/bootjava/config/AppConfig.java
+++ src/main/java/ru/javaops/bootjava/config/AppConfig.java
@@ -5,6 +5,7 @@ import org.h2.tools.Server;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import java.sql.SQLException;
 
@@ -12,8 +13,10 @@ import java.sql.SQLException;
 @Slf4j
 @EnableCaching
 public class AppConfig {
+
+    @Profile("!test")
     @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server h2Server() throws SQLException {
+    Server h2Server() throws SQLException {
         log.info("Start H2 TCP server");
         return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
     }
