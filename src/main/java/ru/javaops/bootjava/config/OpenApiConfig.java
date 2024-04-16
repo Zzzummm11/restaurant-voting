d9e88d85e1c073bee714dcad565ed495d2e5d9fe
Konstantin Mogilev <zzzummm272@gmail.com>
2024-04-17 00:06:53 +0400

    updated contacts

--- src/main/java/ru/javaops/bootjava/config/OpenApiConfig.java
+++ src/main/java/ru/javaops/bootjava/config/OpenApiConfig.java
@@ -28,7 +28,7 @@ import org.springframework.context.annotation.Configuration;
                         - admin@gmail.com / admin<br>
                         - guest@gmail.com / guest</p>
                         """,
-                contact = @Contact(url = "https://javaops.ru/#contacts", name = "Grigory Kislin", email = "admin@javaops.ru")
+                contact = @Contact(name = "Konstantin Mogilev", email = "zzzummm272@gmail.com")
         ),
         security = @SecurityRequirement(name = "basicAuth")
 )
